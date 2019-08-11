17ac1f3b680a86376c9777925f05a5f365fb553b
Gustavo1518 <gmeneses137@gmail.com>
2019-08-11 13:09:12 -0500

    se creo repositorio

---
+++ src/main/webapp/app/core/home/home.component.ts
@@ -0,0 +1,25 @@
+import Component from 'vue-class-component';
+import { Inject, Vue } from 'vue-property-decorator';
+import LoginService from '@/account/login.service';
+import  { HelloWorldComponent }  from '@conacyt/hello-world/src';
+
+
+
+@Component
+export default class Home extends Vue {
+  @Inject('loginService')
+  private loginService: () => LoginService;
+
+  public openLogin(): void {
+    this.loginService().openLogin((<any>this).$root);
+  }
+
+  public get authenticated(): boolean {
+    return this.$store.getters.authenticated;
+  }
+
+  public get username(): string {
+    return this.$store.getters.account ? this.$store.getters.account.login : '';
+  }
+}
+Vue.component('form-conacyt', HelloWorldComponent);
---
+++ src/main/webapp/app/core/home/home.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="home row">
+        <div class="col-md-3">
+            <span class="hipster img-fluid rounded"></span>
+        </div>
+        <div class="col-md-9">
+            <h1 class="display-4">Welcomennnnnn!</h1>
+            <p class="lead">This is your homepage</p>
+
+            <div>
+                <div class="alert alert-success" v-if="authenticated">
+                    <span v-if="username">You are logged in as user "{{username}}"</span>
+                </div>
+
+                <div class="alert alert-warning" v-if="!authenticated">
+                    <span>If you want to </span>
+                    <a class="alert-link" v-on:click="openLogin()">sign in</a><span >, you can try the default accounts:<br/>- Administrator (login="admin" and password="admin") <br/>- User (login="user" and password="user").</span>
+                </div>
+                <div class="alert alert-warning" v-if="!authenticated">
+                    <span>You don't have an account yet?</span>&nbsp;
+                    <router-link class="alert-link" to="/register">Register a new account</router-link>
+                </div>
+            </div>
+
+            <p>
+                If you have any question on JHipster:
+            </p>
+        </div>
+        <h1>Formulario</h1>
+        <form-conacyt></form-conacyt>
+    </div>
+</template>
+
+<script lang="ts" src="./home.component.ts">
+</script>
